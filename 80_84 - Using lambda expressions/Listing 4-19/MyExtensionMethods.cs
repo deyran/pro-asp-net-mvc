ac15903f8d33c089e3942c3539052e3f07d0d44e
Deyvid Rannyere <deyran@gmail.com>
2021-05-06 16:09:42 -0300

    Listing 4-19. Using delegate in an extendion method in the MyExtensionMethods.cs file -> 80
    >> USING LAMBDA EXPRESSIONS -> 80-84
    >>>> Listing 4-19. Using delegate in an extendion method in the MyExtensionMethods.cs file -> 80

---
+++ 80_84 - Using lambda expressions/Listing 4-19/MyExtensionMethods.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+namespace LanguageFeatures.Models
+{
+    /*
+    >> USING LAMBDA EXPRESSIONS -> 80-84
+    >>>> Listing 4-19. Using delegate in an extendion method in the MyExtensionMethods.cs file -> 80    
+    */
+    public static class MyExtensionMethods
+    {
+        public static decimal TotalPrices(this IEnumerable<Product> productEnum)
+        {
+            decimal total = 0;
+            foreach(Product prod in productEnum)
+            {
+                total += prod.Price;
+            }
+            return total;
+        }
+
+        public static IEnumerable<Product> FilterByCategory(this IEnumerable<Product> productEnum, string categoryParam)
+        {
+            foreach(Product prod in productEnum)
+            {
+                if(prod.Category == categoryParam)
+                {
+                    yield return prod;
+                }
+            }
+        }
+   
+        public static IEnumerable<Product> Filter(this IEnumerable<Product> productEnum, Func<Product, bool> selectorParam)
+        {
+            foreach(Product prod in productEnum)
+            {
+                if (selectorParam(prod))
+                {
+                    yield return prod;
+                }
+            }
+        }
+    }
+}
