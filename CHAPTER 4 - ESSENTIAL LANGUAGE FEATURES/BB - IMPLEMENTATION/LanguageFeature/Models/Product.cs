055b57c364d316fdc2e16ba2e171c0bd4cb19e66
Deyvid Rannyere <deyran@gmail.com>
2021-05-11 11:54:15 -0300

    Listing 4-7. Reverting from an automatic to a regular property in the Product.cs file
    >> USING AUTOMATICALLY IMPLEMENTED PROPERTIES
    >>>> Listing 4-7. Reverting from an automatic to a regular property in the Product.cs file

--- CHAPTER 4 - ESSENTIAL LANGUAGE FEATURES/BB - IMPLEMENTATION/LanguageFeature/Models/Product.cs
+++ CHAPTER 4 - ESSENTIAL LANGUAGE FEATURES/BB - IMPLEMENTATION/LanguageFeature/Models/Product.cs
@@ -3,12 +3,17 @@ namespace LanguageFeature.Models
 {
     /*
     >> USING AUTOMATICALLY IMPLEMENTED PROPERTIES
-    >>>> Listing 4-6. Using automatically implemented properties in the Product.cs file
+    >>>> Listing 4-7. Reverting from an automatic to a regular property in the Product.cs file
     */
     public class Product
     {
+        private string name;
         public int ProductID { get; set; }
-        public string Name { get; set; }
+        public string Name
+        {
+            get { return ProductID + name; }
+            set { name = value; }
+        }
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Category { set; get; }
