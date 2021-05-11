289643990db7e28f763445d4dd798f4e615b1440
Deyvid Rannyere <deyran@gmail.com>
2021-05-11 13:59:40 -0300

    Listing 4-11 - The shoppingCart class in the ShoppingCart.cs file
    >> USING EXTENSION METHODS - 74-80
    >>>> Listing 4-11 - The shoppingCart class in the ShoppingCart.cs file

---
+++ CHAPTER 4 - ESSENTIAL LANGUAGE FEATURES/BB - IMPLEMENTATION/LanguageFeature/Models/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageFeature.Models
+{
+    /*
+    >> USING EXTENSION METHODS - 74-80
+    >>>> Listing 4-11 - The shoppingCart class in the ShoppingCart.cs file
+     */
+    public class ShoppingCart
+    {
+        public List<Product> Products { get; set; }
+    }
+}
