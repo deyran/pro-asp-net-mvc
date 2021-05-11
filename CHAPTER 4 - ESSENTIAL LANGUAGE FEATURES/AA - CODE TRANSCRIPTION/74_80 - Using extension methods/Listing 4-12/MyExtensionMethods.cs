11128ce2e4665445cfafa62aff901b51f6ec6a95
Deyvid Rannyere <deyran@gmail.com>
2021-05-11 14:17:01 -0300

    Listing 4-12 - Defining and extension method in the MyExtensionMethods.cs file
    >> USING EXTENSION METHODS - 74-80
    >>>> Listing 4-12 - Defining and extension method in the MyExtensionMethods.cs file

--- CHAPTER 4 - ESSENTIAL LANGUAGE FEATURES/AA - CODE TRANSCRIPTION/74_80 - Using extension methods/Listing 4-12/MyExtensionMethods.cs
+++ CHAPTER 4 - ESSENTIAL LANGUAGE FEATURES/AA - CODE TRANSCRIPTION/74_80 - Using extension methods/Listing 4-12/MyExtensionMethods.cs
@@ -4,14 +4,17 @@ namespace LanguageFeature.Models
     >> USING EXTENSION METHODS - 74-80
     >>>> Listing 4-12 - Defining and extension method in the MyExtensionMethods.cs file
     */
-    public static decimal TotalPrices(this ShoppingCart cartParam)
+    public static class MyExtensionMethods
     {
-        decimal total = 0;
-        foreach(Product pro in cartParam.Products)
+        public static decimal TotalPrices(this ShoppingCart cartParam)
         {
-            total += prod.Price;
-        }
+            decimal total = 0;
+            foreach(Product pro in cartParam.Products)
+            {
+                total += prod.Price;
+            }
 
-        return total;
+            return total;
+        }
     }
 }
---
+++ CHAPTER 4 - ESSENTIAL LANGUAGE FEATURES/BB - IMPLEMENTATION/LanguageFeature/Models/MyExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using LanguageFeature.Models;
+
+namespace LanguageFeature.Models
+{
+	/*
+	>> USING EXTENSION METHODS - 74-80
+	>>>> Listing 4-12 - Defining and extension method in the MyExtensionMethods.cs file
+	 */
+	public static class MyExtensionMethods
+    {
+		public static decimal TotalPrices(this ShoppingCart cartParam)
+		{
+			decimal total = 0;
+			foreach (Product prod in cartParam.Products)
+			{
+                total += prod.Price;
+			}
+
+			return total;
+		}
+	}
+}
