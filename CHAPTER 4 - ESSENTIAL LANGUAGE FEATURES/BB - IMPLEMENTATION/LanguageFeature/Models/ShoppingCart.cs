d5606d56e52c35f7a1582f4a88f763f34f47c1bc
Deyvid Rannyere <deyran@gmail.com>
2021-05-11 14:48:59 -0300

    Listing 4-14 - Implementing an interface in the ShoppingCart.cs file -> 76
    >> USING EXTENSION METHODS -> 74-80
    >>>> Applying extension methods to an interface -> 76
    >>>>>>>> Listing 4-14 - Implementing an interface in the ShoppingCart.cs file -> 76

--- CHAPTER 4 - ESSENTIAL LANGUAGE FEATURES/BB - IMPLEMENTATION/LanguageFeature/Models/ShoppingCart.cs
+++ CHAPTER 4 - ESSENTIAL LANGUAGE FEATURES/BB - IMPLEMENTATION/LanguageFeature/Models/ShoppingCart.cs
@@ -1,13 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace LanguageFeature.Models
 {
     /*
-    >> USING EXTENSION METHODS - 74-80
-    >>>> Listing 4-11 - The shoppingCart class in the ShoppingCart.cs file
+    >> USING EXTENSION METHODS -> 74-80
+    >>>> Applying extension methods to an interface -> 76
+    >>>>>>>> Listing 4-14 - Implementing an interface in the ShoppingCart.cs file -> 76    
      */
-    public class ShoppingCart
+    public class ShoppingCart : IEnumerable<Product>
     {
         public List<Product> Products { get; set; }
+
+        public IEnumerator<Product> GetEnumerator()
+        {
+            return Products.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
