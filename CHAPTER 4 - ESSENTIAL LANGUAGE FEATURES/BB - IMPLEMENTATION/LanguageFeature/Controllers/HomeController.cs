32b768a8aca27cddea43947eb5fbacd051b9f1cc
Deyvid Rannyere <deyran@gmail.com>
2021-05-11 13:44:35 -0300

    Listing 4-9. Using the object Initializer feature in the HomeController.cs file
    >> USING OBJECT AND COLLECTION INITIALIZES
    >>>> Listing 4-9. Using the object Initializer feature in the HomeController.cs file

--- CHAPTER 4 - ESSENTIAL LANGUAGE FEATURES/BB - IMPLEMENTATION/LanguageFeature/Controllers/HomeController.cs
+++ CHAPTER 4 - ESSENTIAL LANGUAGE FEATURES/BB - IMPLEMENTATION/LanguageFeature/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace LanguageFeature.Controllers
 {
     /*
     >> USING OBJECT AND COLLECTION INITIALIZES
-    >>>> Listing 4-8. Constructing and initializing an object with properties in the HomeController.cs file
+    >>>> Listing 4-9. Using the object Initializer feature in the HomeController.cs file
     */
 
     public class HomeController : Controller
@@ -15,21 +15,21 @@ namespace LanguageFeature.Controllers
         {
             return "Navigate to a URL to show an example";
         }
-      /*  public ViewResult AutoProperty()
-        {
-            // ... statements omitted for brevity ...
-        }*/
+        /*  public ViewResult AutoProperty()
+          {
+              // ... statements omitted for brevity ...
+          }*/
         public ViewResult CreateProduct()
         {
-            // create a new Product object
-            Product myProduct = new Product();
-
-            // set the property value
-            myProduct.ProductID = 100;
-            myProduct.Name = "Kayak";
-            myProduct.Description = "A boat for one person";
-            myProduct.Price = 275M;
-            myProduct.Category = "Watersports";
+            // create and populate a new Product object
+            Product myProduct = new Product
+            {
+                ProductID = 100,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Price = 275M,
+                Category = "Watersports"
+            };
 
             return View(
                 "Result",
