5ee535f822512bab805a1cff42df9b5f67419179
Deyvid Rannyere <deyran@gmail.com>
2021-05-11 11:44:57 -0300

    Listing 4-4. Consuming a Property in the HomeController.cs file (page 70)
    >> USING AUTOMATICALLY IMPLEMENTED PROPERTIES
    >>>> Listing 4-4. Consuming a Property in the HomeController.cs file (page 70)

--- CHAPTER 4 - ESSENTIAL LANGUAGE FEATURES/BB - IMPLEMENTATION/LanguageFeature/Controllers/HomeController.cs
+++ CHAPTER 4 - ESSENTIAL LANGUAGE FEATURES/BB - IMPLEMENTATION/LanguageFeature/Controllers/HomeController.cs
@@ -1,17 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using LanguageFeature.Models;
 
 namespace LanguageFeature.Controllers
 {
+    /*
+    >> USING AUTOMATICALLY IMPLEMENTED PROPERTIES
+    >>>> Listing 4-4. Consuming a Property in the HomeController.cs file (page 70)
+    */
+
     public class HomeController : Controller
     {
-        // GET: Home
-        public ActionResult Index()
+        public string Index()
         {
-            return View();
+            return "Navigate to a URL to show an example";
         }
+
+        public ViewResult AutoProperty()
+        {
+            // create a new Product object
+            Product myProduct = new Product();
+
+            // set the property value
+            myProduct.Name = "Kayak";
+
+            // get the property
+            string productName = myProduct.Name;
+
+            // generate the view
+            return View("Result", (object)String.Format("Product name: {0}", productName));
+        }
+
     }
 }
