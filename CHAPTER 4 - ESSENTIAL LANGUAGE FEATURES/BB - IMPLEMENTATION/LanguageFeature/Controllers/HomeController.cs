edf54e93ac2f0f70f60d9e795b7397ed2bd1ff38
Deyvid Rannyere <deyran@gmail.com>
2021-05-14 11:46:16 -0300

    Listing 4-27. Querying without LINQ in the HomeController.cs file -> 86
    >> PERFORMING LANGUAGE INTEGRATED QUERIES -> 86-91
    >>>> Listing 4-27. Querying without LINQ in the HomeController.cs file -> 86

--- CHAPTER 4 - ESSENTIAL LANGUAGE FEATURES/BB - IMPLEMENTATION/LanguageFeature/Controllers/HomeController.cs
+++ CHAPTER 4 - ESSENTIAL LANGUAGE FEATURES/BB - IMPLEMENTATION/LanguageFeature/Controllers/HomeController.cs
@@ -7,8 +7,8 @@ using LanguageFeature.Models;
 namespace LanguageFeature.Controllers
 {
     /*
-    >> USING ANONUMOUS TYPE -> 84
-    >>>> Listing 4-26. Creating an array of anonymously typed object in the HomeController.cs file -> 85    
+    >> PERFORMING LANGUAGE INTEGRATED QUERIES -> 86-91
+    >>>> Listing 4-27. Querying without LINQ in the HomeController.cs file -> 86    
      */
 
     public class HomeController : Controller
@@ -145,6 +145,45 @@ namespace LanguageFeature.Controllers
             foreach (var item in oddsAndEnds)
             {
                 result.Append(item.Name).Append("");
+            }
+
+            return View
+            (
+                "Result",
+                (object)result.ToString()
+            );
+        }
+
+        public ViewResult FindProducts()
+        {
+            Product[] products =
+            {
+                new Product { Name = "Kayak", Category = "Watersports", Price = 275M},
+                new Product { Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+                new Product { Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+                new Product { Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+            };
+
+            // define the array to hold the results
+            Product[] foundProducts = new Product[3];
+
+            // sort the contents of the array
+            Array.Sort
+            (
+                products, (item1, item2) =>
+                {
+                    return Comparer<decimal>.Default.Compare(item1.Price, item2.Price);
+                }
+            );
+
+            // get the first three items in the array as the results
+            Array.Copy(products, foundProducts, 3);
+
+            // create the result
+            StringBuilder result = new StringBuilder();
+            foreach (Product p in foundProducts)
+            {
+                result.AppendFormat("Price: {0}", p.Price);
             }
 
             return View
