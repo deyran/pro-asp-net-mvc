409c888941420bd9f026d47ed7b36c4700703eac
Deyvid Rannyere <deyran@gmail.com>
2021-05-19 10:35:42 -0300

    Listing 5-10. Adding a new action method to the HomeController.cs file -> 103
    >> WORKINK WITH LAYOUTS -> 99
    >>>> DEMONSTRATING SHARED LAYOUTS -> 103
    >>>>>>>> Listing 5-10. Adding a new action method to the HomeController.cs file -> 103

---
+++ CHAPTER 5 - WORKING WITH RAZOR/AA - CODE TRANSCRIPTION/CC - WORKINK WITH LAYOUTS/Listing 5-10/HomeController.cs
@@ -0,0 +1,33 @@
+using System.Web.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    /*
+    >> WORKINK WITH LAYOUTS -> 99
+    >>>> DEMONSTRATING SHARED LAYOUTS -> 103
+    >>>>>>>> Listing 5-10. Adding a new action method to the HomeController.cs file -> 103    
+    */
+
+    public class HomeController : Controller
+    {
+        Product myProduct = new Product
+        {
+            ProductID = 1,
+            Name = "Kayak",
+            Description = "A boat for one person",
+            Category = "Watersports",
+            Price = 275M
+        };
+
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+
+        public ActionResult NameAndPrice()
+        {
+            return View(myProduct);
+        }
+    }
+}
