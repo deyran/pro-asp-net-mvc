96ce023610c175d499c59e804cbe552f28a10bcd
Deyvid Rannyere <deyran@gmail.com>
2021-05-21 11:08:01 -0300

    Listing 5-10. Adding a new action method to the HomeController.cs file -> 103
    >> WORKINK WITH LAYOUTS -> 99
    >>>> DEMONSTRATING SHARED LAYOUTS -> 103
    >>>>>>>> Listing 5-10. Adding a new action method to the HomeController.cs file -> 103

--- CHAPTER 5 - WORKING WITH RAZOR/BB - IMPLEMENTATION/Razor/Controllers/HomeController.cs
+++ CHAPTER 5 - WORKING WITH RAZOR/BB - IMPLEMENTATION/Razor/Controllers/HomeController.cs
@@ -7,6 +7,10 @@ namespace Razor.Controllers
     >> PREPARING THE EXAMPLE PROJECT -> 95
     >>>> DEFINING THE MODEL -> 95
     >>>>>>>> Listing 5-2. The content of the HomeController file -> 96     
+
+    >> WORKINK WITH LAYOUTS -> 99
+    >>>> DEMONSTRATING SHARED LAYOUTS -> 103
+    >>>>>>>> Listing 5-10. Adding a new action method to the HomeController.cs file -> 103
     */
     public class HomeController : Controller
     {
@@ -21,6 +25,11 @@ namespace Razor.Controllers
 
         // GET: Home
         public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+
+        public ActionResult NameAndPrice()
         {
             return View(myProduct);
         }
