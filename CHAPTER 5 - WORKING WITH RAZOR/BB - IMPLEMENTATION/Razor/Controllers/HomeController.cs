3df1c66f363e14e17eb8efaee965119774c57734
Deyvid Rannyere <deyran@gmail.com>
2021-05-21 09:32:53 -0300

    Listing 5-2. The content of the HomeController file -> 96
    >> PREPARING THE EXAMPLE PROJECT -> 95
    >>>> DEFINING THE MODEL -> 95
    >>>>>>>> Listing 5-2. The content of the HomeController file -> 96

--- CHAPTER 5 - WORKING WITH RAZOR/BB - IMPLEMENTATION/Razor/Controllers/HomeController.cs
+++ CHAPTER 5 - WORKING WITH RAZOR/BB - IMPLEMENTATION/Razor/Controllers/HomeController.cs
@@ -1,26 +1,28 @@
+﻿using Razor.Models;
 using System.Web.Mvc;
-using Razor.Models;
 
 namespace Razor.Controllers
 {
     /*
     >> PREPARING THE EXAMPLE PROJECT -> 95
     >>>> DEFINING THE MODEL -> 95
-    >>>>>>>> Listing 5-2. The content of the HomeController file -> 96    
+    >>>>>>>> Listing 5-2. The content of the HomeController file -> 96     
     */
     public class HomeController : Controller
     {
-        Product myProduct = new Product
-        {
+        Product myProduct = new Product 
+        { 
             ProductID = 1,
             Name = "Kayak",
             Description = "A boat for one person",
             Category = "Watersports",
             Price = 275M
-        };        
+        };
+
+        // GET: Home
         public ActionResult Index()
         {
-            return View(myProduct); 
-        }        
+            return View();
+        }
     }
 }
--- CHAPTER 5 - WORKING WITH RAZOR/BB - IMPLEMENTATION/Razor/Models/Product.cs
+++ CHAPTER 5 - WORKING WITH RAZOR/BB - IMPLEMENTATION/Razor/Models/Product.cs
@@ -8,10 +8,10 @@ namespace Razor.Models
     /*
     >> PREPARING THE EXAMPLE PROJECT -> 95
     >>>> DEFINING THE MODEL -> 95
-    >>>>>>>> Listing 5-1. The contents of the Products.cs file -> 96
+    >>>>>>>> Listing 5-1. The contents of the Product.cs file -> 96
      */
 
-    public class Products
+    public class Product
     {
         public int ProductID { get; set; }
         public string Name { get; set; }
