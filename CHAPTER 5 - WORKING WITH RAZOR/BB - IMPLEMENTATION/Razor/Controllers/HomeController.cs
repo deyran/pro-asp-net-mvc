7552f8e7641c366ec16f45bb35b7d9e647179c19
Deyvid Rannyere <deyran@gmail.com>
2021-05-27 15:09:56 -0300

    Listing 5-19. The contents of the DemoArray.cshtml file -> 116
    >> USING RAZOR EXPRESSIONS -> 107
    >>>> ENUMERATING ARRAYS AND COLLECTIONS -> 115
    >>>>>>>> Listing 5-19. The contents of the DemoArray.cshtml file -> 116

--- CHAPTER 5 - WORKING WITH RAZOR/BB - IMPLEMENTATION/Razor/Controllers/HomeController.cs
+++ CHAPTER 5 - WORKING WITH RAZOR/BB - IMPLEMENTATION/Razor/Controllers/HomeController.cs
@@ -5,16 +5,22 @@ namespace Razor.Controllers
 {
     /*
     >> PREPARING THE EXAMPLE PROJECT -> 95
+
     >>>> DEFINING THE MODEL -> 95
     >>>>>>>> Listing 5-2. The content of the HomeController file -> 96     
+    ---------------------------------------------------------------------------------------------------------
 
     >> WORKINK WITH LAYOUTS -> 99
     >>>> DEMONSTRATING SHARED LAYOUTS -> 103
     >>>>>>>> Listing 5-10. Adding a new action method to the HomeController.cs file -> 103
+    ---------------------------------------------------------------------------------------------------------
 
     >> USING RAZOR EXPRESSIONS -> 107
     >>>> INSERTING DATA VALUES -> 107
     >>>>>>>> Listing 5-13. The DemoExpression action method in the HomeController.cs file -> 107
+
+    >>>> ENUMERATING ARRAYS AND COLLECTIONS -> 115
+    >>>>>>>> Listing 5-18. The DemoArray action method in the HomeController file -> 115
     */
     public class HomeController : Controller
     {
@@ -46,6 +52,19 @@ namespace Razor.Controllers
             ViewBag.Supplier = null;
 
             return View(myProduct);
+        }
+
+        public ActionResult DemoArray()
+        {
+            Product[] array =
+            {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Coner flag", Price = 34.95M}
+            };
+
+            return View(array);
         }
     }
 }
