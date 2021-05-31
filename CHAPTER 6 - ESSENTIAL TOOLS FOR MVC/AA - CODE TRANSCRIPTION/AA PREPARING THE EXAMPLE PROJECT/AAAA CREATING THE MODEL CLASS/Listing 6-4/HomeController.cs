3760b1fd2c5dfcf0eea1927b41235ce54ef13d97
Deyvid Rannyere <deyran@gmail.com>
2021-05-31 15:52:50 -0300

    Listing 6-4. The contents of the HomeController.cs file -> 122
    AA PREPEARING THE EXAMPLE PROJECT -> 120
    AABB ADDING THE CONTROLLER -> 121
    >> Listing 6-4. The contents of the HomeController.cs file -> 122

---
+++ CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/AA - CODE TRANSCRIPTION/AA PREPARING THE EXAMPLE PROJECT/AAAA CREATING THE MODEL CLASS/Listing 6-4/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Web.Mvc;
+using System.Linq;
+using EssentialTools.Models;
+
+namespace EssentialTools.Controllers
+{
+    /*
+    AA PREPEARING THE EXAMPLE PROJECT -> 120
+    AABB ADDING THE CONTROLLER -> 121
+    >> Listing 6-4. The contents of the HomeController.cs file -> 122    
+    */
+    public class HomeController : Controller
+    {
+        private Product[] products =
+        {
+            new Product { Name = "Kayak", Category = "Watersports", Price = 275M },
+            new Product { Name = "Lifejacket", Category = "Watersports", Price = 48.95M },
+            new Product { Name = "Soccer ball", Category = "Soccer", Price = 19.50M },
+            new Product { Name = "Corner flag", Category = "Soccer", Price = 34.95M }
+        };
+
+        public ActionResult Index()
+        {
+            LinqValueCalculator calc = new LinqValueCalculator();
+            ShoppingCart cart = new ShoppingCart(calc) { Products = products };
+            decimal totalValue = cart.CalculateProductTotal();
+
+            return View(totalValue);
+        }
+    }
+}
