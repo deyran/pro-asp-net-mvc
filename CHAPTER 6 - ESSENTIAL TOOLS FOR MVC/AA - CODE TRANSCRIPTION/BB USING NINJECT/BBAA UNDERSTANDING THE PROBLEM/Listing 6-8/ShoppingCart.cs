3ba8f4ded3491b7adc2c2fefbd35a5cafcf2fd86
Deyvid Rannyere <deyran@gmail.com>
2021-06-01 15:58:06 -0300

    Listing 6-8. Applying the interface in the ShoppingCart.cs -> 124
    BB USING NINJECT -> 123
    BBAA UNDERSTANDING THE PROBLEM -> 123
    >> APPLYING AN INTERFACE -> 123
    >>>> Listing 6-8. Applying the interface in the ShoppingCart.cs -> 124

---
+++ CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/AA - CODE TRANSCRIPTION/BB USING NINJECT/BBAA UNDERSTANDING THE PROBLEM/Listing 6-8/ShoppingCart.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+
+namespace EssentialTools.Models
+{
+    /*
+    BB USING NINJECT -> 123
+    BBAA UNDERSTANDING THE PROBLEM -> 123
+    >> APPLYING AN INTERFACE -> 123
+    >>>> Listing 6-8. Applying the interface in the ShoppingCart.cs -> 124
+    */
+    public class ShoppingCart
+    {
+        private IValueCalculator calc;
+        public ShoppingCart(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+        public IEnumerable<Product> Products { get; set; }
+        public decimal CalculateProductTotal()
+        {
+            return calc.ValueProducts(Products);
+        }
+    }    
+}
