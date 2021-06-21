d9595a8a9d16ac9cea37d55f170cfc3ad1d06e4e
Deyvid Rannyere <deyran@gmail.com>
2021-06-21 15:50:16 -0300

    Listing 6-26. The contents of the MinimumDiscountHelper.cs -> 137
    CC UNIT TESTING WITH VISUAL STUDIO -> 137
    >> Listing 6-26. The contents of the MinimumDiscountHelper.cs -> 137

---
+++ CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/AA - CODE TRANSCRIPTION/CC UNIT TESTING WITH VISUAL STUDIO/Listing 6-26/MinimumDiscountHelper.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace EssentialTools.Models
+{
+    public class MinimumDiscountHelper : IDiscountHelper
+    {
+        public decimal ApplyDiscount(decimal totalParam)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
