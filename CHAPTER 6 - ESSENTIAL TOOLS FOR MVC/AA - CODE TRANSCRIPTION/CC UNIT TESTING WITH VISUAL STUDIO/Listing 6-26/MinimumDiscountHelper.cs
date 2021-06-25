e1c5cd0d00c1d66b19c7a582356b66041e7ee08c
Deyvid Rannyere <deyran@gmail.com>
2021-06-25 15:32:34 -0300

    Listing 6-27. Adding the Test methods to the UnitTest1.cs file -> 139
    CC UNIT TESTING WITH VISUAL STUDIO -> 137
    CCBB CREATING THE UNIT TESTS -> 139
    >> Listing 6-27. Adding the Test methods to the UnitTest1.cs file -> 139

--- CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/AA - CODE TRANSCRIPTION/CC UNIT TESTING WITH VISUAL STUDIO/Listing 6-26/MinimumDiscountHelper.cs
+++ CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/AA - CODE TRANSCRIPTION/CC UNIT TESTING WITH VISUAL STUDIO/Listing 6-26/MinimumDiscountHelper.cs
@@ -2,6 +2,10 @@ using System;
 
 namespace EssentialTools.Models
 {
+    /*
+    CC UNIT TESTING WITH VISUAL STUDIO -> 137
+    >> Listing 6-26. The contents of the MinimumDiscountHelper.cs -> 137
+    */
     public class MinimumDiscountHelper : IDiscountHelper
     {
         public decimal ApplyDiscount(decimal totalParam)
