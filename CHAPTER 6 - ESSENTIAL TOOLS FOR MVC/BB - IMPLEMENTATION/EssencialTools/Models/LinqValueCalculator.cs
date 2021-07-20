1096851ef9ce323bf4e6cd5023cfcb4c3f22746e
Deyvid Rannyere <deyran@gmail.com>
2021-07-20 17:59:51 -0300

    IMPLEMNTATION -> Listing 6-2. The contents of the LinqValueCalculators.cs file -> 121
        AA PREPEARING THE EXAMPLE PROJECT -> 120
        AAAA CREATING THE MODEL CLASS -> 120
        >> Listing 6-2. The contents of the LinqValueCalculators.cs file -> 121

---
+++ CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/BB - IMPLEMENTATION/EssencialTools/Models/LinqValueCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EssencialTools.Models
+{
+    /*
+    AA PREPEARING THE EXAMPLE PROJECT -> 120
+    AAAA CREATING THE MODEL CLASS -> 120
+    >> Listing 6-2. The contents of the LinqValueCalculators.cs file -> 121    
+    */
+    public class LinqValueCalculator
+    {
+        public decimal ValueProducts(IEnumerable<Product> products)
+        {
+            return products.Sum(p => p.Price);
+        }
+    }
+}
