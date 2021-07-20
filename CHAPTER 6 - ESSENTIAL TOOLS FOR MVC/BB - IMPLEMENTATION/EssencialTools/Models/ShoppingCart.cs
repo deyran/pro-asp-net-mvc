c0c4b0c596a0b9103adef823cf9b495a44dcdb66
Deyvid Rannyere <deyran@gmail.com>
2021-07-20 18:09:21 -0300

    IMPLEMENTATION -> Listing 6-3. The contents of the ShoppingCart.cs file -> 121
    AA PREPEARING THE EXAMPLE PROJECT -> 120
    AAAA CREATING THE MODEL CLASS -> 120
    >> Listing 6-3. The contents of the ShoppingCart.cs file -> 121

--- CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/BB - IMPLEMENTATION/EssencialTools/Models/ShoppingCart.cs
+++ CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/BB - IMPLEMENTATION/EssencialTools/Models/ShoppingCart.cs
@@ -1,12 +1,7 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
-namespace EssentialTools.Models
+namespace EssencialTools.Models
 {
-    /*
-    AA PREPEARING THE EXAMPLE PROJECT -> 120
-    AAAA CREATING THE MODEL CLASS -> 120
-    >> Listing 6-3. The contents of the ShoppingCart.cs file -> 121
-    */
     public class ShoppingCart
     {
         private LinqValueCalculator calc;
@@ -17,7 +12,7 @@ namespace EssentialTools.Models
         }
 
         public IEnumerable<Product> Products { get; set; }
-        
+
         public decimal CalculateProductTotal()
         {
             return calc.ValueProducts(Products);
