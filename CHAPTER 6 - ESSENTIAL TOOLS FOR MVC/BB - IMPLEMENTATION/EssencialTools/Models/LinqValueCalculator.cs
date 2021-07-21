1ca2e7f0cf71734a2bd1d0628ce373728cfd86bd
Deyvid Rannyere <deyran@gmail.com>
2021-07-21 17:29:14 -0300

    Implementation => Listing 6-7. Applying an interface in the LinqValueCalculator.cs file -> 124
    BB USING NINJECT -> 123
    BBAA UNDERSTANDING THE PROBLEM -> 123
    >> APPLYING AN INTERFACE -> 123
    >>>> Listing 6-7. Applying an interface in the LinqValueCalculator.cs file -> 124

--- CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/BB - IMPLEMENTATION/EssencialTools/Models/LinqValueCalculator.cs
+++ CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/BB - IMPLEMENTATION/EssencialTools/Models/LinqValueCalculator.cs
@@ -6,9 +6,14 @@ namespace EssencialTools.Models
     /*
     AA PREPEARING THE EXAMPLE PROJECT -> 120
     AAAA CREATING THE MODEL CLASS -> 120
-    >> Listing 6-2. The contents of the LinqValueCalculators.cs file -> 121    
+    >> Listing 6-2. The contents of the LinqValueCalculators.cs file -> 121
+
+    BB USING NINJECT -> 123
+    BBAA UNDERSTANDING THE PROBLEM -> 123
+    >> APPLYING AN INTERFACE -> 123
+    >>>> Listing 6-7. Applying an interface in the LinqValueCalculator.cs file -> 124    
     */
-    public class LinqValueCalculator
+    public class LinqValueCalculator: IValueCalculator
     {
         public decimal ValueProducts(IEnumerable<Product> products)
         {
