5a4085c1adc89b7a1a33aac3be0ee586283aa64a
Deyvid Rannyere <deyran@gmail.com>
2021-07-22 17:57:13 -0300

    Listing 6-10. Adding the basic Ninject functionality to the index action method in the HomeController.cs file -> 125
    BB USING NINJECT -> 123
    BBCC GETTING STARTED WITH NINJECT -> 125
    >> Listing 6-10. Adding the basic Ninject functionality to the index action method in the HomeController.cs file -> 125

---
+++ CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/AA - CODE TRANSCRIPTION/BB USING NINJECT/BBCC GETTING STARTED WITH NINJECT/Listing 6-10/HomeController.cs
@@ -0,0 +1,36 @@
+using System.Web.Mvc;
+using EssentialTools.Models;
+using Ninject;
+
+namespace EssentialTools.Controllers
+{
+    /*
+    BB USING NINJECT -> 123
+    BBCC GETTING STARTED WITH NINJECT -> 125
+    >> Listing 6-10. Adding the basic Ninject functionality to the index action method in the HomeController.cs file -> 125    
+    */
+    public class HomeController : Controller
+    {
+        private Product[] products =
+        {
+            new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+            new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+            new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+            new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+        };
+
+        public ActionResult Index()
+        {
+            IKernel ninjectKernel = new StandardKernel();
+            ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+
+            IValueCalculator calc = ninjectKernel.Get<IValueCalculator>();
+
+            ShoppingCart cart = new ShoppingCart(calc) { Products = products };
+
+            decimal totalValue = cart.CalculateProductTotal();
+
+            return View(totalValue);
+        }
+    }
+}
