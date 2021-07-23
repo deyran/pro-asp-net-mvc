df13c45736d49e86277fc54564e6c2dda5fbb101
Deyvid Rannyere <deyran@gmail.com>
2021-07-23 14:58:58 -0300

    Listing 6-10. Adding the basic Ninject functionality to the index action method in the HomeController.cs file -> 125
    BB USING NINJECT -> 123
    BBCC GETTING STARTED WITH NINJECT -> 125
    >> Listing 6-10. Adding the basic Ninject functionality to the index action method in the HomeController.cs file -> 125

--- CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/BB - IMPLEMENTATION/EssencialTools/Controllers/HomeController.cs
+++ CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/BB - IMPLEMENTATION/EssencialTools/Controllers/HomeController.cs
@@ -1,17 +1,22 @@
 ﻿using System.Web.Mvc;
 using EssencialTools.Models;
+using Ninject;
 
 namespace EssencialTools.Controllers
 {
     /*
     AA PREPEARING THE EXAMPLE PROJECT -> 120
-    AABB ADDING THE CONTROLLER -> 121
-    >> Listing 6-4. The contents of the HomeController.cs file -> 122    
+        AABB ADDING THE CONTROLLER -> 121
+        >> Listing 6-4. The contents of the HomeController.cs file -> 122    
 
     BB USING NINJECT -> 123
-    BBAA UNDERSTANDING THE PROBLEM -> 123
-    >> APPLYING AN INTERFACE -> 123
-    >>>> Listing 6-9. Applying the interface to the HomeController.cs -> 124    
+        BBAA UNDERSTANDING THE PROBLEM -> 123
+            >> APPLYING AN INTERFACE -> 123
+            >>>> Listing 6-9. Applying the interface to the HomeController.cs -> 124    
+
+        BBCC GETTING STARTED WITH NINJECT -> 125
+            >> Listing 6-10. Adding the basic Ninject functionality to the index action method in the HomeController.cs file -> 125    
+
     */
 
     public class HomeController : Controller
@@ -27,11 +32,17 @@ namespace EssencialTools.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            IValueCalculator calc = new LinqValueCalculator();
+            IKernel ninjectKernel = new StandardKernel();
+            ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+
+            IValueCalculator calc = ninjectKernel.Get<IValueCalculator>();
+
             ShoppingCart cart = new ShoppingCart(calc) { Products = products };
+
             decimal totalValue = cart.CalculateProductTotal();
 
             return View(totalValue);
+
         }
     }
 }
