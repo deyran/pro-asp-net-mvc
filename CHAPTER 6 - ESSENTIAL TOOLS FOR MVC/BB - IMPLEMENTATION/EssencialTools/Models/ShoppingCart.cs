8e5c429b09566fd0b256f060f095bca19bbc746b
Deyvid Rannyere <deyran@gmail.com>
2021-07-26 14:30:25 -0300

    Implemantation
    Implemantation

--- CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/BB - IMPLEMENTATION/EssencialTools/Models/ShoppingCart.cs
+++ CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/BB - IMPLEMENTATION/EssencialTools/Models/ShoppingCart.cs
@@ -10,14 +10,13 @@ namespace EssencialTools.Models
     public class ShoppingCart
     {
         private IValueCalculator calc;
+        public IEnumerable<Product> Products { get; set; }
 
         public ShoppingCart(IValueCalculator calcParam)
         {
             calc = calcParam;
         }
-
-        public IEnumerable<Product> Products { get; set; }
-
+        
         public decimal CalculateProductTotal()
         {
             return calc.ValueProducts(Products);
