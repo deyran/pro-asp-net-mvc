e728e5bdb7c24cab3d01d9c392d028ed899dddec
Deyvid Rannyere <deyran@gmail.com>
2021-07-27 15:56:41 -0300

    Implementation >> Listing 6-14. The contents of the Discount.cs file -> 130
    BB USING NINJECT -> 123
    BBEE CREATING CHAINS OF DEPENDENCY -> 130
    >> Listing 6-14. The contents of the Discount.cs file -> 130

---
+++ CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/BB - IMPLEMENTATION/EssencialTools/Models/Discount.cs
@@ -0,0 +1,21 @@
+﻿namespace EssencialTools.Models
+{
+    /*
+    BB USING NINJECT -> 123
+    BBEE CREATING CHAINS OF DEPENDENCY -> 130
+    >> Listing 6-14. The contents of the Discount.cs file -> 130    
+    */
+
+    public interface IDiscountHelper
+    {
+        decimal ApplyDiscount(decimal totalParam);
+    }
+
+    public class DefaultDiscountHelper : IDiscountHelper
+    {
+        public decimal ApplyDiscount(decimal totalParam)
+        {
+            return (totalParam - (10m / 100m * totalParam));
+        }
+    }
+}
