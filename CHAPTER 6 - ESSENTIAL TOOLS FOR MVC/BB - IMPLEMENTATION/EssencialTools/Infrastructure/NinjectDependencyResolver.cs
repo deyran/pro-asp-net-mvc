e99bf4fd5674e064ced8b37309a8abdf2d49e9c9
Deyvid Rannyere <deyran@gmail.com>
2021-07-27 16:27:02 -0300

    Implementation >> Listing 6-16. Binding another interface to its implementation in the NinjectDependencyResolver.cs file -> 131
    BB USING NINJECT -> 123
    BBEE CREATING CHAINS OF DEPENDENCY -> 130
    >> Listing 6-16. Binding another interface to its implementation in the NinjectDependencyResolver.cs file -> 131

--- CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/BB - IMPLEMENTATION/EssencialTools/Infrastructure/NinjectDependencyResolver.cs
+++ CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/BB - IMPLEMENTATION/EssencialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -8,9 +8,12 @@ namespace EssencialTools.Infrastructure
 {
     /*
     BB USING NINJECT -> 123
-    BBDD SETTING UP MVC DEPENDENCY INJECTION -> 127
-        >> CREATING THE DEPENDENCY RESOLVER -> 127
-            >>>> Listing 6-11. The content of the NinjectDependencyResolver.cs file -> 127    
+        BBDD SETTING UP MVC DEPENDENCY INJECTION -> 127
+            >> CREATING THE DEPENDENCY RESOLVER -> 127
+                >>>> Listing 6-11. The content of the NinjectDependencyResolver.cs file -> 127
+    
+        BBEE CREATING CHAINS OF DEPENDENCY -> 130
+            >> Listing 6-16. Binding another interface to its implementation in the NinjectDependencyResolver.cs file -> 131
     */
 
     public class NinjectDependencyResolver : IDependencyResolver
@@ -36,6 +39,7 @@ namespace EssencialTools.Infrastructure
         private void AddBinding()
         {
             kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+            kernel.Bind<IDiscountHelper>().To<DefaultDiscountHelper>();
         }
     }
 }
