218c43fdd380480d53750146333261ead9a53a0c
Deyvid Rannyere <deyran@gmail.com>
2021-07-27 16:40:28 -0300

    Implementation >> Listing 6-17. Adding a property in the Discount.cs file -> 131
    BB USING NINJECT -> 123
    BBFF SPECIFYING PROPERTY AND CONSTRUCTOR PARAMETER VALUES -> 131
    >> Listing 6-17. Adding a property in the Discount.cs file -> 131

--- CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/BB - IMPLEMENTATION/EssencialTools/Models/Discount.cs
+++ CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/BB - IMPLEMENTATION/EssencialTools/Models/Discount.cs
@@ -2,8 +2,11 @@
 {
     /*
     BB USING NINJECT -> 123
-    BBEE CREATING CHAINS OF DEPENDENCY -> 130
-    >> Listing 6-14. The contents of the Discount.cs file -> 130    
+        BBEE CREATING CHAINS OF DEPENDENCY -> 130
+            >> Listing 6-14. The contents of the Discount.cs file -> 130
+
+        BBFF SPECIFYING PROPERTY AND CONSTRUCTOR PARAMETER VALUES -> 131
+            >> Listing 6-17. Adding a property in the Discount.cs file -> 131
     */
 
     public interface IDiscountHelper
@@ -13,9 +16,11 @@
 
     public class DefaultDiscountHelper : IDiscountHelper
     {
+        public decimal DiscountSize { get; set; }
+
         public decimal ApplyDiscount(decimal totalParam)
         {
-            return (totalParam - (10m / 100m * totalParam));
+            return (totalParam - (DiscountSize / 100m * totalParam));
         }
     }
 }
