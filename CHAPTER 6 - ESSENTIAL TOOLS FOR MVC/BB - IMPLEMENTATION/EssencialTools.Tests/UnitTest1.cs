ca87103805b3ae442c53cf3e9bd5e642119df9d0
Deyvid Rannyere <deyran@gmail.com>
2021-08-11 15:10:35 -0300

    Implementation >> Listing 6-27. Adding the Test methods to the UnitTest1.cs file -> 139
    CC UNIT TESTING WITH VISUAL STUDIO -> 137
    CCBB CREATING THE UNIT TESTS -> 139
    >> Listing 6-27. Adding the Test methods to the UnitTest1.cs file -> 139

--- CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/BB - IMPLEMENTATION/EssencialTools.Tests/UnitTest1.cs
+++ CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/BB - IMPLEMENTATION/EssencialTools.Tests/UnitTest1.cs
@@ -1,14 +1,35 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssencialTools.Models;
 
-namespace EssencialTools.Tests
+namespace EssentialTools.Test
 {
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
-    }
+	[TestClass]
+	public class UnitTest1
+	{
+		/*
+		CC UNIT TESTING WITH VISUAL STUDIO -> 137
+		CCBB CREATING THE UNIT TESTS -> 139
+		>> Listing 6-27. Adding the Test methods to the UnitTest1.cs file -> 139    
+		 */
+
+		private IDiscountHelper getTestObject()
+		{
+			return new MinimumDiscountHelper();
+		}
+
+		[TestMethod]
+		public void Discount_Above_100()
+		{
+			// arrange
+			IDiscountHelper target = getTestObject();
+			decimal total = 200;
+
+			// act
+			var discountedTotal = target.ApplyDiscount(total);
+
+			// assert
+			Assert.AreEqual(total * 0.9M, discountedTotal);
+		}
+	}
 }
