14a39dbca9694f15256a0342b163b929c4922484
Deyvid Rannyere <deyran@gmail.com>
2021-08-16 15:44:05 -0300

    Implementation >> Listing 6-29. The contents of the MinimumDiscountHelper.cs file -> 143
    CC UNIT TESTING WITH VISUAL STUDIO -> 137
    CCCC RUNNING THE UNIT TESTS (AND FAILING) -> 142
    >> IMPLEMENTING THE FEATURE -> 143
    >>>> Listing 6-29. The contents of the MinimumDiscountHelper.cs file -> 143

--- CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/BB - IMPLEMENTATION/EssencialTools/Models/MinimumDiscountHelper.cs
+++ CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/BB - IMPLEMENTATION/EssencialTools/Models/MinimumDiscountHelper.cs
@@ -4,13 +4,32 @@ namespace EssencialTools.Models
 {
     /*
     CC UNIT TESTING WITH VISUAL STUDIO -> 137
-    >> Listing 6-26. The contents of the MinimumDiscountHelper.cs -> 137
+        >> Listing 6-26. The contents of the MinimumDiscountHelper.cs -> 137
+
+    CCCC RUNNING THE UNIT TESTS (AND FAILING) -> 142
+        >> IMPLEMENTING THE FEATURE -> 143
+            >>>> Listing 6-29. The contents of the MinimumDiscountHelper.cs file -> 143
     */
     public class MinimumDiscountHelper : IDiscountHelper
     {
         public decimal ApplyDiscount(decimal totalParam)
         {
-            throw new NotImplementedException();
+            if (totalParam < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else if (totalParam > 100)
+            {
+                return totalParam * 0.9M;
+            }
+            else if (totalParam > 10 && totalParam <= 100)
+            {
+                return totalParam - 5;
+            }
+            else
+            {
+                return totalParam;
+            }
         }
     }
 }
