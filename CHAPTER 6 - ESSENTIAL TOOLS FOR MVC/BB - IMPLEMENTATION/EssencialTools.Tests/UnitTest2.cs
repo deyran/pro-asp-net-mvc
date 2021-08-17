9e5b24aad73ed627a920f8fd08a6eb1d9b22d3ce
Deyvid Rannyere <deyran@gmail.com>
2021-08-17 17:26:45 -0300

    DDAA UNDERSTANDING THE PROBLEM -> 146
    DD USING MOQ -> 145
    DDAA UNDERSTANDING THE PROBLEM -> 146

---
+++ CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/BB - IMPLEMENTATION/EssencialTools.Tests/UnitTest2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssencialTools.Models;
+using System.Linq;
+
+namespace EssencialTools.Tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
