d4515df62d82bb627c1e3b20e29dfc3137f348db
Deyvid Rannyere <deyran@gmail.com>
2021-08-19 15:33:58 -0300

    Implementation
    DD USING MOQ -> 145
    DDAA UNDERSTANDING THE PROBLEM -> 146
        >> Listing 6-32. Add a Unit Test for the ShoppingCart class in the UnitTest2.cs file -> 146
    DDBB ADDING MOQ TO THE VISUAL STUDIO PROJECT -> 147

--- CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/BB - IMPLEMENTATION/EssencialTools.Tests/UnitTest2.cs
+++ CHAPTER 6 - ESSENTIAL TOOLS FOR MVC/BB - IMPLEMENTATION/EssencialTools.Tests/UnitTest2.cs
@@ -8,6 +8,29 @@ namespace EssencialTools.Tests
     [TestClass]
     public class UnitTest2
     {
+        private Product[] products =
+        {
+            new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+            new Product {Name = "Life", Category = "Watersports", Price = 48.95M},
+            new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+            new Product {Name = "Soccer", Category = "Soccer", Price = 34.95M}
+        };
+
+        [TestMethod]
+        public void Sum_Products_Correctly()
+        {
+            // arrange
+            var discounter = new MinimumDiscountHelper();
+            var target = new LinqValueCalculator(discounter);
+            var goalTotal = products.Sum(e => e.Price);
+
+            // act
+            var result = target.ValueProducts(products);
+
+            // assert
+            Assert.AreEqual(goalTotal, result);
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
