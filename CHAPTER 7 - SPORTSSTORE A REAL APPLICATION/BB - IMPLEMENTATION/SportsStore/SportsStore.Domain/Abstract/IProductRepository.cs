53f5d62ec91f7c6e6ede3b69ba52e376de8a2206
Deyvid Rannyere <deyran@gmail.com>
2021-10-05 15:49:10 -0300

    Listing 7-4. The contents of the IProductRepository.cs file.
    BB STARTING THE DOMAIN MODEL
    	CREATING AN ABSTRACT REPOSITORY
    		Listing 7-4. The contents of the IProductRepository.cs file.

--- CHAPTER 7 - SPORTSSTORE A REAL APPLICATION/BB - IMPLEMENTATION/SportsStore/SportsStore.Domain/Abstract/IProductRepository.cs
+++ CHAPTER 7 - SPORTSSTORE A REAL APPLICATION/BB - IMPLEMENTATION/SportsStore/SportsStore.Domain/Abstract/IProductRepository.cs
@@ -3,8 +3,14 @@ using SportsStore.Domain.Entities;
 
 namespace SportsStore.Domain.Abstract
 {
+    /*
+    BB STARTING THE DOMAIN MODEL
+	    CREATING AN ABSTRACT REPOSITORY
+		    Listing 7-4. The contents of the IProductRepository.cs file.
+     */
+
     public interface IProductRepository
     {
-        public IEnumerable<Product> Products { get; }
+        IEnumerable<Product> Products { get; }
     }
 }
