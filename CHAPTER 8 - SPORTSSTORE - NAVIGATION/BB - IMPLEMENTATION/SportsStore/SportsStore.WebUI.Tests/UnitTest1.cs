878c89ab20bc2e8f5efc5c0fc3caa5f9d0a51c71
Deyvid Rannyere <deyran@gmail.com>
2022-01-25 15:43:03 -0300

    Update

--- CHAPTER 8 - SPORTSSTORE - NAVIGATION/BB - IMPLEMENTATION/SportsStore/SportsStore.WebUI.Tests/UnitTest1.cs
+++ CHAPTER 8 - SPORTSSTORE - NAVIGATION/BB - IMPLEMENTATION/SportsStore/SportsStore.WebUI.Tests/UnitTest1.cs
@@ -22,6 +22,10 @@ namespace SportsStore.WebUI.Tests
     ADDING NAVIGATION CONTROLS
 	    FILTERING THE PRODUCT LIST
 		    UNIT TEST: CATEGORY FILTERING
+    
+	    BUILDING A CATEGORY NAVIGATION MENU
+		    GENERATING CATEGORY LISTS
+			    UNIT TEST: GENERATION THE CATEGORY LIST
      */
 
     [TestClass]
@@ -149,6 +153,34 @@ namespace SportsStore.WebUI.Tests
             Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
             Assert.IsTrue(result[1].Name == "P4" && result[1].Category == "Cat2");
 
+        }
+
+        public void Can_Create_Categories()
+        {
+            // Arrange
+            // - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(
+                new Product[]
+                {
+                    new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+                    new Product {ProductID = 2, Name = "P2", Category = "Apples"},
+                    new Product {ProductID = 3, Name = "P3", Category = "Plums"},
+                    new Product {ProductID = 4, Name = "P4", Category = "Oranges"}
+                }
+            );
+
+            // Arrange - create the controller
+            NavController target = new NavController(mock.Object);
+
+            // Act = get the set of categories
+            string[] results = ((IEnumerable<string>)target.Menu().Model).ToArray();
+
+            // Assert
+            Assert.AreEqual(results.Length, 3);
+            Assert.AreEqual(results[0], "Apples");
+            Assert.AreEqual(results[1], "Oranges");
+            Assert.AreEqual(results[2], "Plums");
         }
 
         [TestMethod]
