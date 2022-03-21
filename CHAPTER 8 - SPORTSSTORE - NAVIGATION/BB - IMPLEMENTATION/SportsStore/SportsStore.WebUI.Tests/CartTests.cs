02a1f6f793a3e206960d75b39770a05b11c2a715
Deyvid Rannyere <deyran@gmail.com>
2022-03-21 16:10:05 -0300

    UNIT TEST: TESTING THE CART - REMOVE PRODUCT
    BUILDING THE SHOPPING CART
    	DEFINING THE CART ENTITY
    		UNIT TEST: TESTING THE CART - REMOVE PRODUCT

--- CHAPTER 8 - SPORTSSTORE - NAVIGATION/BB - IMPLEMENTATION/SportsStore/SportsStore.WebUI.Tests/CartTests.cs
+++ CHAPTER 8 - SPORTSSTORE - NAVIGATION/BB - IMPLEMENTATION/SportsStore/SportsStore.WebUI.Tests/CartTests.cs
@@ -8,6 +8,7 @@ namespace SportsStore.WebUI.Tests
      BUILDING THE SHOPPING CART
         DEFINING THE CART ENTITY
             UNIT TEST: TESTING THE CART
+            UNIT TEST: TESTING THE CART - REMOVE PRODUCT
      */
 
     [TestClass]
@@ -31,6 +32,32 @@ namespace SportsStore.WebUI.Tests
             Assert.AreEqual(results.Length, 2);
             Assert.AreEqual(results[0].Product, p1);
             Assert.AreEqual(results[1].Product, p2);
+
+        }
+
+        [TestMethod]
+        public void Can_Remove_Line()
+        {
+            // Arrange = create some test products
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+            Product p3 = new Product { ProductID = 3, Name = "P3" };
+
+            // Arrange = create a new cart
+            Cart target = new Cart();
+
+            // Arrange - add some products to the cart
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 3);
+            target.AddItem(p3, 5);
+            target.AddItem(p2, 1);
+
+            // Act
+            target.RemoveLine(p2);
+
+            // Assert
+            Assert.AreEqual(target.Lines.Where(c => c.Product == p2).Count(), 0);
+            Assert.AreEqual(target.Lines.Count(), 2);
 
         }
     }
