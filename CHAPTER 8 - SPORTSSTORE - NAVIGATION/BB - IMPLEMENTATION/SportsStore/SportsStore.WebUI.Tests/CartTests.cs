19f77435f8124f92ee1eb4eadca4540152f251c7
Deyvid Rannyere <deyran@gmail.com>
2022-03-24 14:16:37 -0300

    UNIT TEST: TESTING THE CART - ADD QUANTITY FOR EXISTING LINE
    BUILDING THE SHOPPING CART
    	DEFINING THE CART ENTITY
    		UNIT TEST: TESTING THE CART - ADD QUANTITY FOR EXISTING LINE

--- CHAPTER 8 - SPORTSSTORE - NAVIGATION/BB - IMPLEMENTATION/SportsStore/SportsStore.WebUI.Tests/CartTests.cs
+++ CHAPTER 8 - SPORTSSTORE - NAVIGATION/BB - IMPLEMENTATION/SportsStore/SportsStore.WebUI.Tests/CartTests.cs
@@ -8,6 +8,7 @@ namespace SportsStore.WebUI.Tests
      BUILDING THE SHOPPING CART
         DEFINING THE CART ENTITY
             UNIT TEST: TESTING THE CART
+            UNIT TEST: TESTING THE CART - ADD QUANTITY FOR_EXISTING_LINE
             UNIT TEST: TESTING THE CART - REMOVE PRODUCT
      */
 
@@ -33,7 +34,29 @@ namespace SportsStore.WebUI.Tests
             Assert.AreEqual(results.Length, 2);
             Assert.AreEqual(results[0].Product, p1);
             Assert.AreEqual(results[1].Product, p2);
+        }
 
+        [TestMethod]
+        public void Can_Add_Quantity_For_Existing_Lines() 
+        {
+            // Arrange - create some test products
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+
+            // Arrange - create a new car
+            Cart target = new Cart();
+
+            // Act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 10);
+
+            CartLine[] results = target.Lines.OrderBy(c => c.Product.ProductID).ToArray();
+
+            // Assert
+            Assert.AreEqual(results.Length, 2);
+            Assert.AreEqual(results[0].Quantity, 11);
+            Assert.AreEqual(results[1].Quantity, 1);
         }
 
         [TestMethod]
