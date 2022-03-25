117b69df6cf1d3491a529f69ae617161f454ab54
Deyvid Rannyere <deyran@gmail.com>
2022-03-25 15:04:01 -0300

    Listing 8-14. The Contents of the CartController.cs File
    BUILDING THE SHOPPING CART
    	IMPLEMENTING THE CART CONTROLLER
    		Listing 8-14. The Contents of the CartController.cs File

---
+++ CHAPTER 8 - SPORTSSTORE - NAVIGATION/BB - IMPLEMENTATION/SportsStore/SportsStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Controllers
+{
+	/*
+	BUILDING THE SHOPPING CART
+		IMPLEMENTING THE CART CONTROLLER
+			Listing 8-14. The Contents of the CartController.cs File
+	 */
+
+	public class CartController : Controller
+    {
+		private IProductRepository repository;
+
+		public CartController(IProductRepository repo)
+		{
+			repository = repo;
+		}
+
+		public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+		{
+			Product product = repository.Products
+						.FirstOrDefault(p => p.ProductID == productId);
+
+			if (product != null)
+			{
+				GetCart().AddItem(product, 1);
+			}
+
+			return RedirectToAction("Index", new { returnUrl });
+		}
+
+		public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+		{
+			Product product = repository.Products
+						.FirstOrDefault(p => p.ProductID == productId);
+
+			if (product != null)
+			{
+				GetCart().RemoveLine(product);
+			}
+
+			return RedirectToAction("Index", new { returnUrl });
+		}
+
+		private Cart GetCart()
+		{
+			Cart cart = (Cart)Session["Cart"];
+
+			if (cart == null)
+			{
+				cart = new Cart();
+				Session["Cart"] = cart;
+			}
+
+			return cart;
+		}
+
+	}
+}
