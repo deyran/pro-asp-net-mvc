eeb239965fa4a8a9377b5f48f6277a040cab4102
Deyvid Rannyere <deyran@gmail.com>
2022-04-05 17:08:56 -0300

    Figure 8-9. Displaying the contents of the shopping cart
    BUILDING THE SHOPPING CART
    	DISPLAYING THE CONTENTS OF THE CART
    		Figure 8-9. Displaying the contents of the shopping cart
    
    Error

--- CHAPTER 8 - SPORTSSTORE - NAVIGATION/BB - IMPLEMENTATION/SportsStore/SportsStore.WebUI/Controllers/CartController.cs
+++ CHAPTER 8 - SPORTSSTORE - NAVIGATION/BB - IMPLEMENTATION/SportsStore/SportsStore.WebUI/Controllers/CartController.cs
@@ -1,13 +1,12 @@
-﻿using System.Linq;
-using System.Web.Mvc;
-using SportsStore.Domain.Abstract;
+﻿using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
-
 using SportsStore.WebUI.Models;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace SportsStore.WebUI.Controllers
 {
-	/*
+    /*
 	BUILDING THE SHOPPING CART
 		IMPLEMENTING THE CART CONTROLLER
 			Listing 8-14. The Contents of the CartController.cs File
@@ -16,66 +15,65 @@ namespace SportsStore.WebUI.Controllers
 			Listing 8-16. The Index Action Method in the CartController.cs File
 	 */
 
-	public class CartController : Controller
+    public class CartController : Controller
     {
-		private IProductRepository repository;
+        private IProductRepository repository;
 
-		public CartController(IProductRepository repo)
-		{
-			repository = repo;
-		}
+        public CartController(IProductRepository repo)
+        {
+            repository = repo;
+        }
 
-		public ViewResult Index(string returnUrl)
-		{
-			return (
-				View (
-					new CartIndexViewModel
-					{
-						Cart = GetCart(),
-						ReturnUrl = returnUrl
-					}
-				)
-			);
-		}
+        public ViewResult Index(string returnUrl)
+        {
+            return (
+                View(
+                    new CartIndexViewModel
+                    {
+                        Cart = GetCart(),
+                        ReturnUrl = returnUrl
+                    }
+                )
+            );
+        }
 
-		public RedirectToRouteResult AddToCart(int productId, string returnUrl)
-		{
-			Product product = repository.Products
-						.FirstOrDefault(p => p.ProductID == productId);
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products
+               .FirstOrDefault(p => p.ProductID == productId);
 
-			if (product != null)
-			{
-				GetCart().AddItem(product, 1);
-			}
+            if (product != null)
+            {
+                GetCart().AddItem(product, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
 
-			return RedirectToAction("Index", new { returnUrl });
-		}
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products
+                        .FirstOrDefault(p => p.ProductID == productId);
 
-		public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
-		{
-			Product product = repository.Products
-						.FirstOrDefault(p => p.ProductID == productId);
+            if (product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
 
-			if (product != null)
-			{
-				GetCart().RemoveLine(product);
-			}
+            return RedirectToAction("Index", new { returnUrl });
+        }
 
-			return RedirectToAction("Index", new { returnUrl });
-		}
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
 
-		private Cart GetCart()
-		{
-			Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
 
-			if (cart == null)
-			{
-				cart = new Cart();
-				Session["Cart"] = cart;
-			}
+            return cart;
+        }
 
-			return cart;
-		}
-
-	}
+    }
 }
