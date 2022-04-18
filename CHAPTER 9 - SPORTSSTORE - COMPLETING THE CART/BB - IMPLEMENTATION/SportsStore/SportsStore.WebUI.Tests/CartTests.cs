6dbe4d79fd8dbbaff615dbfe3588f510ca015e0d
Deyvid Rannyere <deyran@gmail.com>
2022-04-18 17:16:39 -0300

    UNIT TEST: THE CART CONTROLLER
    USING MODEL BINDING
    	CREATING A CUSTOM MODEL BINDER
    		UNIT TEST: THE CART CONTROLLER

--- CHAPTER 9 - SPORTSSTORE - COMPLETING THE CART/BB - IMPLEMENTATION/SportsStore/SportsStore.WebUI.Tests/CartTests.cs
+++ CHAPTER 9 - SPORTSSTORE - COMPLETING THE CART/BB - IMPLEMENTATION/SportsStore/SportsStore.WebUI.Tests/CartTests.cs
@@ -2,6 +2,12 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SportsStore.Domain.Entities;
 
+using Moq;
+using System.Web.Mvc;
+using SportsStore.WebUI.Models;
+using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Controllers;
+
 namespace SportsStore.WebUI.Tests
 {
     /*
@@ -12,11 +18,87 @@ namespace SportsStore.WebUI.Tests
             UNIT TEST: TESTING THE CART - REMOVE PRODUCT
             UNIT TEST: TESTING THE CART - CALCULATE THE TOTAL
             UNIT TEST: TESTING THE CART - CLEAR CONTENTS OF THE CART
+
+    USING MODEL BINDING
+	    CREATING A CUSTOM MODEL BINDER
+		    UNIT TEST: THE CART CONTROLLER
      */
 
     [TestClass]
     public class CartTests
     {
+
+        [TestMethod]
+        public void Can_Add_To_Cart()
+        {
+            // Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(
+                new Product[]
+                {
+                    new Product {ProductID = 1, Name = "P1", Category = "Apples"}
+                }.AsQueryable()
+            );
+
+            // Arrange - create a Cart
+            Cart cart = new Cart();
+
+            // Arrange - crate the controller
+            CartController target = new CartController(mock.Object);
+
+            // Act - add a product to the cart
+            target.AddToCart(cart, 1, null);
+
+            // Assert
+            Assert.AreEqual(cart.Lines.Count(), 1);
+            Assert.AreEqual(cart.Lines.ToArray()[0].Product.ProductID, 1);
+        }
+
+        [TestMethod]
+        public void Adding_Product_To_Cart_Goes_To_Cart_Screen()
+        {
+            // Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(
+                new Product[]
+                {
+                    new Product {ProductID = 1, Name = "P1", Category = "Apples"}
+                }.AsQueryable()
+            );
+
+            // Arrange - create a Cart
+            Cart cart = new Cart();
+
+            // Arrange - create the controller
+            CartController target = new CartController(mock.Object);
+
+            // Act - Add a product to the cart
+            RedirectToRouteResult result = target.AddToCart(cart, 2, "myUrl");
+
+            // Assert
+            Assert.AreEqual(result.RouteValues["action"], "Index");
+            Assert.AreEqual(result.RouteValues["returnUrl"], "myUrl");
+        }
+
+        [TestMethod]
+        public void Can_View_Cart_Contents()
+        {
+            // Arrange - create a Cart
+            Cart cart = new Cart();
+
+            // Arrange - create the controller
+            CartController target = new CartController(null);
+
+            // Act - call the index action method
+            CartIndexViewModel result
+            = (CartIndexViewModel)target.Index(cart, "myUrl").ViewData.Model;
+
+            // Assert
+            Assert.AreSame(result.Cart, cart);
+            Assert.AreEqual(result.ReturnUrl, "myUrl");
+        }
+
+
         [TestMethod]
         public void Can_Add_New_Lines()
         {
