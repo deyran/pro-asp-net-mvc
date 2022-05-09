dd795adcfb9e2fd869d5bf40ffd53e3da29c896c
Deyvid Rannyere <deyran@gmail.com>
2022-05-09 13:51:55 -0300

    Listing 9-15. The Contents of the EmailOrderProcessor.cs File
    SUBMITTING ORDERS
    	IMPLEMENTING THE ORDER PROCESSOR 244
    		IMPLEMENTING THE INTERFACE
    			Listing 9-15. The Contents of the EmailOrderProcessor.cs File

---
+++ CHAPTER 9 - SPORTSSTORE - COMPLETING THE CART/BB - IMPLEMENTATION/SportsStore/SportsStore.Domain/Concrete/EmailOrderProcessor.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Text;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Concrete
+{
+	/*
+    SUBMITTING ORDERS	
+	    IMPLEMENTING THE ORDER PROCESSOR 244
+		    IMPLEMENTING THE INTERFACE
+			    Listing 9-15. The Contents of the EmailOrderProcessor.cs File
+     */
+
+	public class EmailSettings
+	{
+		public string MailToAddress = "deyran@gmail.com";
+		public string MailFromAdress = "sportsstore@example.com";
+		public bool UseSsl = true;
+		public string Username = "deyran";
+		public string Password = "MySmtpPassword";
+		public string ServerName = "smtp.example.com";
+		public int ServerPort = 587;
+		public bool WriteAsFile = false;
+		public string FileLocation = @"c:\sports_store_emails";
+	}
+
+	public class EmailOrderProcessor : IOrderProcessor
+    {
+        public void ProcessOrder(Cart cart, ShippingDetails shippingDetails)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
