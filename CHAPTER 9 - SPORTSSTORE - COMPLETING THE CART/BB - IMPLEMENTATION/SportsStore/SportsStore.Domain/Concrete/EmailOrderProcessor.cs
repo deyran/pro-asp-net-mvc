a5a627206b455734bd8f0023967c1315881d49b4
Deyvid Rannyere <deyran@gmail.com>
2022-05-09 14:40:25 -0300

    Listing 9-15. The Contents of the EmailOrderProcessor.cs File
    SUBMITTING ORDERS
    	IMPLEMENTING THE ORDER PROCESSOR 244
    		IMPLEMENTING THE INTERFACE
    			Listing 9-15. The Contents of the EmailOrderProcessor.cs File

--- CHAPTER 9 - SPORTSSTORE - COMPLETING THE CART/BB - IMPLEMENTATION/SportsStore/SportsStore.Domain/Concrete/EmailOrderProcessor.cs
+++ CHAPTER 9 - SPORTSSTORE - COMPLETING THE CART/BB - IMPLEMENTATION/SportsStore/SportsStore.Domain/Concrete/EmailOrderProcessor.cs
@@ -6,31 +6,93 @@ using SportsStore.Domain.Entities;
 
 namespace SportsStore.Domain.Concrete
 {
-	/*
+    /*
     SUBMITTING ORDERS	
 	    IMPLEMENTING THE ORDER PROCESSOR 244
 		    IMPLEMENTING THE INTERFACE
 			    Listing 9-15. The Contents of the EmailOrderProcessor.cs File
      */
 
-	public class EmailSettings
-	{
-		public string MailToAddress = "deyran@gmail.com";
-		public string MailFromAdress = "sportsstore@example.com";
-		public bool UseSsl = true;
-		public string Username = "deyran";
-		public string Password = "MySmtpPassword";
-		public string ServerName = "smtp.example.com";
-		public int ServerPort = 587;
-		public bool WriteAsFile = false;
-		public string FileLocation = @"c:\sports_store_emails";
-	}
-
-	public class EmailOrderProcessor : IOrderProcessor
+    public class EmailSettings
     {
-        public void ProcessOrder(Cart cart, ShippingDetails shippingDetails)
+        public string MailToAddress = "deyran@gmail.com";
+        public string MailFromAdress = "sportsstore@example.com";
+        public bool UseSsl = true;
+        public string Username = "deyran";
+        public string Password = "MySmtpPassword";
+        public string ServerName = "smtp.example.com";
+        public int ServerPort = 587;
+        public bool WriteAsFile = false;
+        public string FileLocation = @"c:\sports_store_emails";
+    }
+
+    public class EmailOrderProcessor : IOrderProcessor
+    {
+        private EmailSettings emailSettings;
+
+        public EmailOrderProcessor(EmailSettings settings)
         {
-            throw new System.NotImplementedException();
+            emailSettings = settings;
+        }
+        public void ProcessOrder(Cart cart, ShippingDetails shippingInfo)
+        {
+            using (var smtpClient = new SmtpClient())
+            {
+                smtpClient.EnableSsl = emailSettings.UseSsl;
+                smtpClient.Host = emailSettings.ServerName;
+                smtpClient.Port = emailSettings.ServerPort;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential(emailSettings.Username, emailSettings.Password);
+
+                if (emailSettings.WriteAsFile)
+                {
+                    smtpClient.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+                    smtpClient.PickupDirectoryLocation = emailSettings.FileLocation;
+                    smtpClient.EnableSsl = false;
+                }
+
+                StringBuilder body = new StringBuilder()
+                    .AppendLine("A new order has been submitted")
+                    .AppendLine("---")
+                    .AppendLine("Items:");
+
+                foreach (var line in cart.Lines)
+                {
+                    var subtotal = line.Product.Price * line.Quantity;
+                    body.AppendFormat("{0} x {1} (subtotal: {2:c}", line.Quantity,
+                    line.Product.Name,
+                    subtotal);
+                }
+
+                body.AppendFormat("Total order value: {0:c}", cart.ComputeTotalValue())
+                    .AppendLine("---")
+                    .AppendLine("Ship to:")
+                    .AppendLine(shippingInfo.Name)
+                    .AppendLine(shippingInfo.Line1)
+                    .AppendLine(shippingInfo.Line2 ?? "")
+                    .AppendLine(shippingInfo.Line3 ?? "")
+                    .AppendLine(shippingInfo.City)
+                    .AppendLine(shippingInfo.State ?? "")
+                    .AppendLine(shippingInfo.Country)
+                    .AppendLine(shippingInfo.Zip)
+                    .AppendLine("---")
+                    .AppendFormat("Gift wrap: {0}", shippingInfo.GifWrap ? "Yes" : "No");
+
+                MailMessage mailMessage = new MailMessage
+                (
+                    emailSettings.MailFromAdress,  // From
+                    emailSettings.MailToAddress,    // To
+                    "New order submitted!",         // Subject
+                    body.ToString()                 // Body
+                );
+
+                if (emailSettings.WriteAsFile)
+                {
+                    mailMessage.BodyEncoding = Encoding.ASCII;
+                }
+
+                smtpClient.Send(mailMessage);
+            }
         }
     }
 }
