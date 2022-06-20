f11bf16d93a85b31e3c7d551d6166edfe6b39f78
Deyvid Rannyere <deyran@gmail.com>
2022-06-20 14:28:51 -0300

    UNIT TEST: THE EDIT ACTION METHOD
    CHAPTER 11 - SPORTSSTORE - ADMINISTRATION
        ADDING CATALOG MANAGEMENT
            EDITING PRODUCTS
                CREATING THE EDIT ACTION METHOD
                    UNIT TEST: THE EDIT ACTION METHOD

--- CHAPTER 11 - SPORTSSTORE - ADMINIDTRSTION/BB - IMPLEMENTATION/SportsStore/SportsStore.WebUI.Tests/AdminTests.cs
+++ CHAPTER 11 - SPORTSSTORE - ADMINIDTRSTION/BB - IMPLEMENTATION/SportsStore/SportsStore.WebUI.Tests/AdminTests.cs
@@ -20,12 +20,64 @@ namespace SportsStore.WebUI.Tests
             EDITING PRODUCTS
                 CREATING THE EDIT ACTION METHOD
                     UNIT TEST: THE EDIT ACTION METHOD
-                        Listing 11-5A. The Can_Edit_Product test method in the AdminController.cs File
      */
 
     [TestClass]
     public class AdminTests
     {
+
+        [TestMethod]
+        public void Can_Edit_Product()
+        {
+            // Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(
+                new Product[]
+                {
+                    new Product {ProductID = 1, Name = "P1"},
+                    new Product {ProductID = 2, Name = "P2"},
+                    new Product {ProductID = 3, Name = "P3"}
+                }
+            );
+
+            // Arrange - create the controller
+            AdminController target = new AdminController(mock.Object);
+
+            // Act
+            Product p1 = target.Edit(1).ViewData.Model as Product;
+            Product p2 = target.Edit(2).ViewData.Model as Product;
+            Product p3 = target.Edit(3).ViewData.Model as Product;
+
+            // Assert
+            Assert.AreEqual(1, p1.ProductID);
+            Assert.AreEqual(2, p2.ProductID);
+            Assert.AreEqual(3, p3.ProductID);
+        }
+
+        [TestMethod]
+        public void Cannot_Edit_Noneexistent_Product()
+        {
+            // Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(
+                new Product[]
+                {
+                    new Product { ProductID = 1, Name = "P1"},
+                    new Product { ProductID = 2, Name = "P2"},
+                    new Product { ProductID = 3, Name = "P3"}
+                }
+            );
+
+            // Arrange - create the controller
+            AdminController target = new AdminController(mock.Object);
+
+            // Act
+            Product result = (Product)target.Edit(4).ViewData.Model;
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
         [TestMethod]
         public void Index_Contains_All_Products()
         {
