b7f4d7f7aad606051105a3e2997534e7b105042b
Deyvid Rannyere <deyran@gmail.com>
2022-07-13 15:21:29 -0300

    Listing 11-13. Applying Validation Attributes to the Product.cs File
    CHAPTER 11 - SPORTSSTORE - ADMINISTRATION
        ADDING CATALOG MANAGEMENT
            EDITING PRODUCTS
                ADDING MODEL VALIDATION 294
                    Listing 11-13. Applying Validation Attributes to the Product.cs File

--- CHAPTER 11 - SPORTSSTORE - ADMINISTRATION/BB - IMPLEMENTATION/SportsStore/SportsStore.Domain/Entities/Product.cs
+++ CHAPTER 11 - SPORTSSTORE - ADMINISTRATION/BB - IMPLEMENTATION/SportsStore/SportsStore.Domain/Entities/Product.cs
@@ -12,16 +12,27 @@ namespace SportsStore.Domain.Entities
             EDITING PRODUCTS
                 CREATE THE EDIT VIEW
                     Listing 11-7. Using Model Metadata in the Product.cs File
+
+                ADDING MODEL VALIDATION 294
+                    Listing 11-13. Applying Validation Attributes to the Product.cs File
     */
     public class Product
     {
         [HiddenInput(DisplayValue = false)]
         public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a product name")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Please enter a description")]
         [DataType(DataType.MultilineText)]
         public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Please specify a category")]
         public string Category { get; set; }
     }
 }
