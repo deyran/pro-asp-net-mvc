069081536fd0dcefef2febfc1d224b44c765827f
Deyvid Rannyere <deyran@gmail.com>
2022-09-09 15:15:19 -0300

    Listing 12-12. Handling Image Data in the AdminController.cs File
    CHAPTER 12 - SportsStore: Security & Finishing Touches
    # Image Uploads
    ## Saving Images to the Database
    Listing 12-12. Handling Image Data in the AdminController.cs File

--- pro-asp-net-mvc/chapter-12/web-app/SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
+++ pro-asp-net-mvc/chapter-12/web-app/SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System.Web;
+
+using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
 
 using System.Linq;
@@ -29,6 +31,10 @@ namespace SportsStore.WebUI.Controllers
         Securing the Administration Controller
             Applying Authorization with Filters
                 Listing 12-3. Adding the Authorize Attribute in the AdminController.cs File
+
+        # Image Uploads 
+            ## Saving Images to the Database
+                Listing 12-12. Handling Image Data in the AdminController.cs File
      */
 
     [Authorize]
@@ -53,12 +59,20 @@ namespace SportsStore.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Edit(Product product)
+        public ActionResult Edit(Product product, HttpPostedFileBase image = null)
         {
             if (ModelState.IsValid)
             {
+                if(image != null)
+                {
+                    product.ImageMimeType = image.ContentType;
+                    product.ImageData = new byte[image.ContentLength];
+                    image.InputStream.Read(product.ImageData, 0, image.ContentLength);
+                }
+
                 repository.SaveProduct(product);
                 TempData["message"] = string.Format("{0} has been saved", product.Name);
+
                 return RedirectToAction("/Index");
             }
             else
