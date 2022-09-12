dc2c9e96f6f08f0909f92868ec05824833c06cce
Deyvid Rannyere <deyran@gmail.com>
2022-09-12 15:15:23 -0300

    Figure 7-4. Creating the Product class
    # chapter-07-SportsStore-A-Real-Application
        ## Starting the domain model
            Figure 7-4. Creating the Product class

---
+++ pro-asp-net-mvc/chapter-07/web-app/SportsStore/SportsStore.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Entities
+{
+    /*
+    # chapter-07-SportsStore-A-Real-Application
+        ## Starting the domain model
+            Figure 7-4. Creating the Product class
+     */
+    public class Product
+    {
+    }
+}
